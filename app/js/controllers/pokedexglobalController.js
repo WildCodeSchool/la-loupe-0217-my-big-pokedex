be1dc093442d4705f8183c769f65fd222491fde9
Jehan Henry <jehan.henry@gmail.com>
2017-03-27 11:37:43 +0200

    fixed bugs after fifi merge & re-designed button

--- app/js/controllers/pokedexglobalController.js
+++ app/js/controllers/pokedexglobalController.js
@@ -1,38 +1,38 @@
-angular.module('app')
-    .controller('pokedexglobalController', function($scope, pokemonService, $http, LocalService) {
+  angular.module('app')
+      .controller('pokedexglobalController', function($scope, pokemonService, $http, LocalService) {
 
 
-        function localStorage(pokemon) {
-            if (LocalService.get('pokemon') === true) {
-                LocalService.get('pokemon');
-            } else {
-              $http.get('https://pokeapi.co/api/v2/pokemon/');
-                LocalService.set('makey', 'pokemon');
-            }
-        }
+          function localStorage(pokemon) {
+              if (LocalService.get('pokemon') === true) {
+                  LocalService.get('pokemon');
+              } else {
+                  $http.get('https://pokeapi.co/api/v2/pokemon/');
+                  LocalService.set('makey', 'pokemon');
+              }
+          }
 
-        $scope.pokemons = [];
+          $scope.pokemons = [];
 
-        pokemonService.getAll().then(function(res) {
-            $scope.pokemons = res.data.results;
-            $scope.pokemons.forEach(function(pokemon, i) {
-                $http.get(pokemon.url).then(function(res) {
-                    $scope.pokemons[i] = res.data;
-                });
-            });
-            console.log($scope.pokemons);
-            console.log(LocalService.get('makey'));
+          pokemonService.getAll().then(function(res) {
+              $scope.pokemons = res.data.results;
+              $scope.pokemons.forEach(function(pokemon, i) {
+                  $http.get(pokemon.url).then(function(res) {
+                      $scope.pokemons[i] = res.data;
+                  });
+              });
+              console.log($scope.pokemons);
+              console.log(LocalService.get('makey'));
 
 
 
-            // pokemonService.get(id).then(function (res) {
-            //   $scope.pokemon = res.data;
-            // });
-            //
-            // pokemonService.getImage(id).then(function (res) {
-            //   $scope.pokemonImage = res.data;
-            //   console.log();
-            // });
+              // pokemonService.get(id).then(function (res) {
+              //   $scope.pokemon = res.data;
+              // });
+              //
+              // pokemonService.getImage(id).then(function (res) {
+              //   $scope.pokemonImage = res.data;
+              //   console.log();
+              // });
 
-        });
-    });
+          });
+      });
--- app/js/services/pokemonService.js
+++ app/js/services/pokemonService.js
@@ -1,15 +1,15 @@
 angular.module('app')
     .service('pokemonService', function($http) {
-      return {
-          getAll: function(id) {
-            return $http.get('https://pokeapi.co/api/v2/pokemon/');
-          },
-          get: function(id) {
-              return $http.get('https://pokeapi.co/api/v2/pokemon/ + id');
-          },
-          getImage: function(id) {
-              return $http.get('https://pokeapi.co/media/sprites/pokemon/' + id + '.png');
+        return {
+            getAll: function(id) {
+                return $http.get('https://pokeapi.co/api/v2/pokemon/');
+            },
+            get: function(id) {
+                return $http.get('https://pokeapi.co/api/v2/pokemon/ + id');
+            },
+            getImage: function(id) {
+                return $http.get('https://pokeapi.co/media/sprites/pokemon/' + id + '.png');
 
-          }
-      };
-  });
+            }
+        };
+    });
