32cbd96eb6e07cc6f2f921b23963c9d67bf6c7c0
Mana <sabrina.mardjoeki@gmail.com>
2017-03-27 17:06:49 +0200

    mega controller code

--- app/js/controllers/pokedexglobalController.js
+++ app/js/controllers/pokedexglobalController.js
@@ -1,38 +1,13 @@
   angular.module('app')
       .controller('pokedexglobalController', function($scope, pokemonService, $http, LocalService) {
-
-
-          function localStorage(pokemon) {
-              if (LocalService.get('pokemon') === true) {
-                  LocalService.get('pokemon');
-              } else {
-                  $http.get('https://pokeapi.co/api/v2/pokemon/');
-                  LocalService.set('makey', 'pokemon');
-              }
-          }
-
-          $scope.pokemons = [];
-
-          pokemonService.getAll().then(function(res) {
-              $scope.pokemons = res.data.results;
-              $scope.pokemons.forEach(function(pokemon, i) {
-                  $http.get(pokemon.url).then(function(res) {
-                      $scope.pokemons[i] = res.data;
-                  });
-              });
-              console.log($scope.pokemons);
-              console.log(LocalService.get('makey'));
-
-
-
-              // pokemonService.get(id).then(function (res) {
-              //   $scope.pokemon = res.data;
-              // });
-              //
-              // pokemonService.getImage(id).then(function (res) {
-              //   $scope.pokemonImage = res.data;
-              //   console.log();
-              // });
-
+        pokemonService.getAll().then(function(res) {
+          console.log(res.data);
+          $scope.pokemons = res.data.results;
+          $scope.pokemons.map(function(pkmn) {
+              pkmn.id = pkmn.url.match(/([0-9]+)/g)[1];
+              pkmn.img = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + pkmn.id + ".png";
+              return pkmn;
           });
+        });
+
       });
--- app/js/services/pokemonService.js
+++ app/js/services/pokemonService.js
@@ -2,14 +2,7 @@ angular.module('app')
     .service('pokemonService', function($http) {
         return {
             getAll: function(id) {
-                return $http.get('https://pokeapi.co/api/v2/pokemon/');
+                return $http.get('https://pokeapi.co/api/v2/pokemon/?limit=811');
             },
-            get: function(id) {
-                return $http.get('https://pokeapi.co/api/v2/pokemon/ + id');
-            },
-            getImage: function(id) {
-                return $http.get('https://pokeapi.co/media/sprites/pokemon/' + id + '.png');
-
-            }
         };
     });
