7762b9103fe2b779baffc555ea9d0f64000d52c6
Jehan Henry <jehan.henry@gmail.com>
2017-03-31 10:49:28 +0200

    add change from globalController to dashboardController

--- app/js/controllers/dashboardController.js
+++ app/js/controllers/dashboardController.js
@@ -1,5 +1,5 @@
 angular.module('app')
-    .controller('DashboardController', function($scope, pokemonService, $http, LocalService) {
+    .controller('pokedexglobalController', function($scope, pokemonService, $http, LocalService) {
         pokemonService.getAll().then(function(res) {
             console.log(res.data);
             $scope.pokemons = res.data.results;
@@ -8,6 +8,8 @@ angular.module('app')
                 pkmn.img = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + pkmn.id + ".png";
                 return pkmn;
             });
+        }, function(err) {
+            console.log('erreur', err);
         });
         $scope.japName = '';
 
@@ -23,15 +25,56 @@ angular.module('app')
             pokemonService.getSpe(id).then(function(res) {
                 $scope.entrie = (res.data.flavor_text_entries.filter(filterEntrie))[0].flavor_text;
                 $scope.japName = (res.data.names.filter(filterJap))[0].name;
+            }, function(err) {
+                console.log('erreur', err);
             });
         };
+
+        $scope.spinner = true;
+
+        function filterHp(obj) {
+            return obj.stat.url === "https://pokeapi.co/api/v2/stat/1/";
+        }
+        function filterAtt(obj) {
+            return obj.stat.url === "https://pokeapi.co/api/v2/stat/2/";
+        }
+        function filterDef(obj) {
+            return obj.stat.url === "https://pokeapi.co/api/v2/stat/3/";
+        }
+        function filterAts(obj) {
+            return obj.stat.url === "https://pokeapi.co/api/v2/stat/4/";
+        }
+        function filterDfs(obj) {
+            return obj.stat.url === "https://pokeapi.co/api/v2/stat/5/";
+        }
+        function filterSpd(obj) {
+            return obj.stat.url === "https://pokeapi.co/api/v2/stat/6/";
+        }
+
         $scope.getOne = function(id) {
+            $scope.spinner = true;
             pokemonService.getOne(id).then(function(res) {
                 console.log(res.data);
-                $scope.type1 = res.data.types[0].type.name;
-                  if (res.data.types.length > 1) {
-                $scope.type2 = res.data.types[1].type.name;
-              } else $scope.type2 = res.data.types[1].type.name = hidden;
+                $scope.types = res.data.types;
+                $scope.height = res.data.height / 10;
+                $scope.weight = res.data.weight / 10;
+                console.log($scope.types);
+                $scope.spinner = false;
+                $scope.stats = res.data.stats;
+                $scope.hp = ($scope.stats.filter(filterHp))[0].base_stat;
+                $scope.att = ($scope.stats.filter(filterAtt))[0].base_stat;
+                $scope.def = ($scope.stats.filter(filterDef))[0].base_stat;
+                $scope.ats = ($scope.stats.filter(filterAts))[0].base_stat;
+                $scope.dfs = ($scope.stats.filter(filterDfs))[0].base_stat;
+                $scope.spd = ($scope.stats.filter(filterSpd))[0].base_stat;
+            }, function(err) {
+                console.log('erreur', err);
             });
+        };
+
+
+        $scope.clear = function() {
+            $scope.japName = '';
+            $scope.entrie = '';
         };
     });
