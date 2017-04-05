405da27ac902487229daf5558098b84750e9b311
Mana <sabrina.mardjoeki@gmail.com>
2017-04-05 19:43:27 +0200

    update rank method with for loop

--- app/js/controllers/top10.js
+++ app/js/controllers/top10.js
@@ -6,16 +6,9 @@ angular.module('app')
             $scope.allUsers.sort(function(a, b) {
                 return b.pokemonCaught.length - a.pokemonCaught.length;
             });
-            $scope.allUsers[0].rank = 1;
-            $scope.allUsers[1].rank = 2;
-            $scope.allUsers[2].rank = 3;
-            $scope.allUsers[3].rank = 4;
-            $scope.allUsers[4].rank = 5;
-            $scope.allUsers[5].rank = 6;
-            $scope.allUsers[6].rank = 7;
-            $scope.allUsers[7].rank = 8;
-            $scope.allUsers[8].rank = 9;
-            $scope.allUsers[9].rank = 10;
+            for (var i = 0; i < $scope.allUsers.length; i++) {
+                $scope.allUsers[i].rank = i + 1;
+            }
         });
 
     });
